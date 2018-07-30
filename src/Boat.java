94a3cd8a92f7c8f007d929eb85f29ed7d9284d36
Jianyuan Chen <jianyuan.chen12@gmail.com>
2018-07-30 12:10:33 -0300

    finish Boat class

--- src/Boat.java
+++ src/Boat.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public abstract class Boat {
     private int team;
     private Coordinates location;
@@ -5,6 +9,7 @@ public abstract class Boat {
     private int health;
     private int strength;
     private int vision;
+    private final Set<String> DIRECTIONS = new HashSet<>(Arrays.asList("N", "NE", "E", "S", "SE", "W", "NW"));
 
     public Boat(int team, Coordinates location, int direction, int health, int strength, int vision) {
         this.team = team;
@@ -15,11 +20,11 @@ public abstract class Boat {
         this.vision = vision;
     }
 
-    public int getTeam() {
+    private int getTeam() {
         return this.team;
     }
 
-    public Coordinates getLocation() {
+    private Coordinates getLocation() {
         return this.location;
     }
 
@@ -95,6 +100,7 @@ public abstract class Boat {
             // If not occupied:
                 // On the map: Set Boat's current direction as null; Set map's to boat's location; Update boat's location
             // If occupied: return "B1 cannot move to D3 as it is occupied."
+        // If invalid location: return "B1 cannot move off the map."
         Coordinates currentLoc = this.getLocation();
         int x = currentLoc.getX();
         int y = currentLoc.getY();
@@ -102,8 +108,60 @@ public abstract class Boat {
         Coordinates newLoc = world.getAdjacentLocation(currentLoc, this.direction);
         if (world.isLocationValid(newLoc)) {
             if (!world.isLocationOccupied(newLoc)) {
-                world.
+                world.setOccupant(null, currentLoc);
+                world.setOccupant(this, newLoc);
+                return String.format("%s moves to %s", this.getTeam() + this.getID(), newLoc);
+            } else {
+                return String.format("%s cannot move to %s as it is occupied", this.getTeam() + this.getID(), newLoc);
             }
+        } else {
+            return String.format("%s cannot move off the map.", this.getTeam() + this.getID());
         }
+    }
+
+    /**
+     *   @params int -1 if left, +1 if right
+     *   @return a String representing results of action
+     **/
+    public String turn(int leftOrRight) {
+        int newDirection;
+        String result = String.format("%s turned ", this.getTeam() + this.getID());
+
+        if (leftOrRight == -1) {
+            if (this.direction == 0) newDirection = 8 - 1;
+            else                     newDirection = this.direction - 1;
+            result += "left";
+        } else if (leftOrRight == 1) {
+            newDirection = this.direction + 1;
+            result += "right";
+        } else {
+            newDirection = this.direction;
+            return String.format("%s did not turn.", this.getTeam() + this.getID());
+        }
+        result += String.format(", now facing %s", DIRECTIONS.toArray()[this.direction]);
+        return result;
+    }
+
+    public String takeHit(int strengthOfAttack) {
+        int newHealth = this.getHealth() - strengthOfAttack;
+        if (newHealth <= 0) {
+            return String.format("%s has been sunk!", this.getTeam() + this.getID());
+        } else {
+            this.health = newHealth;
+            return String.format("%s takes %d damage", this.getTeam() + this.getID(), strengthOfAttack);
+        }
+    }
+
+    /** assumes input c is valid
+     *
+     * @param c Coordinates (boat's new location)
+     */
+    public void setLocation(Coordinates c) {
+        this.location = c;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s", this.getTeam() + this.getID());
     }
 }
